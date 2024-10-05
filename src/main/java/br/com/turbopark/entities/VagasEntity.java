950fae2889c83553dce39b944a27eb67df5570bc
JvGamer123 <jteixeiramelovieirasilva@gmail.com>
2024-10-05 16:28:47 -0300

    Subindo Projeto

---
+++ src/main/java/br/com/turbopark/entities/VagasEntity.java
@@ -0,0 +1,32 @@
+package br.com.turbopark.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "TB_VAGAS")
+@Table(name = "TB_VAGAS")
+public class VagasEntity {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = true)
+    private String bloco;
+
+    @Column
+    private String statusVaga;
+
+    @Column
+    private Long numeroVaga;
+
+    @Column
+    private boolean coberta;
+}
